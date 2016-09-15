c2971a172e3bf2652a3229f9f63fcb75a3c14845
sebastiaan <sebastiaanvoebal@hotmail.com>
2016-09-15 09:40:25 +0200

    versie 1
    added jump en automatisch naar rechts

---
+++ EndlessRunner/Assets/Jump.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour
+{
+
+
+    private Rigidbody rigidBody;
+    private Vector3 movement;
+    public float speed;
+    bool isFalling;
+    public float jump;
+
+
+    void Awake()
+    {
+
+        rigidBody = GetComponent<Rigidbody>();
+
+
+    }
+  
+    void FixedUpdate()
+    {
+
+        rigidBody.AddForce(movement.normalized * speed);
+        if (Input.GetButton("Jump") && isFalling == false)
+        {
+            rigidBody.velocity = new Vector3(0f, jump, 0f);
+            isFalling = true;
+        }
+
+
+    }
+
+
+
+    void OnCollisionStay()
+    {
+        isFalling = false;
+    }
+}
+
---
+++ EndlessRunner/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    
+  public float movementSpeed = 10f;
+    public Rigidbody rb;
+
+
+    void Start() {
+
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(2F, 0F, 0F);
+        rb.velocity = movement * movementSpeed;
+    }
+}
+
+
+
+
+
+
+
