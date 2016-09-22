18e3f765bef3cb14ed832a65e96ecde74f08e99a
sebastiaan <sebastiaanvoebal@hotmail.com>
2016-09-22 09:24:44 +0200

    changes met platform toevoegen

---
+++ Assets/scripts/PlatformGenarator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenarator : MonoBehaviour {
+
+    public GameObject thePlatform;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    private float platformWidth;
+		void Start () {
+
+        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	void Update () {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            Instantiate(thePlatform, transform.position, transform.rotation);
+        }
+	}
+}
