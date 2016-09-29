f15dcd30135d86c84f1b71c45f680a096dd3d38c
JherSter <jeroenavis123@gmail.com>
2016-09-29 10:12:38 +0200

    Random Platform generator

---
+++ Assets/scripts/DestroyPlatform.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("invis");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnBecameInvisible() {
+		Debug.Log ("invis");
+		Destroy(gameObject);
+	}
+}
--- Assets/scripts/map/PlatformGenarator.cs
+++ Assets/scripts/map/PlatformGenarator.cs
@@ -1,23 +1,53 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class PlatformGenarator : MonoBehaviour {
 
+    public int maxPlatforms = 20;
+    public GameObject platform;
+    public float horizontalMin = 4;
+    public float horizontalMax = 2;
+    public float verticalMin = -6f;
+    public float verticalMax = 6;
     public GameObject thePlatform;
     public Transform generationPoint;
     public float distanceBetween;
+     private float platformWidth;
 
-    private float platformWidth;	
 
-	void Start () { 
-        platformWidth = thePlatform.GetComponent<BoxCollider>().size.x;
-	}
-	
-	void Update () {
+
+    private Vector2 originPosition;
+
+
+    void Start () {
+
+        originPosition = transform.position;
+        Spawn ();
+         platformWidth = thePlatform.GetComponent<BoxCollider>().size.x;
+    
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i < maxPlatforms; i++)
+        {
+            //Vector2 randomPosition = originPosition + new Vector2 (Random.Range(horizontalMin, horizontalMax), Random.Range (verticalMin, verticalMax));
+            //Instantiate(platform, randomPosition, Quaternion.identity);
+            //originPosition = randomPosition;
+        }
+    }
+    void Update () {
         if (transform.position.x < generationPoint.position.x)
         {
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
-            Instantiate(thePlatform, transform.position, transform.rotation);
+			Vector2 randomPosition = originPosition + new Vector2 (Random.Range(horizontalMin, horizontalMax), Random.Range (verticalMin, verticalMax));
+			transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+			Instantiate(platform, randomPosition, transform.rotation);
+			//Instantiate(platform, randomPosition, Quaternion.identity);
+			originPosition = randomPosition;
         }
 	}
+	void OnBecameInvisible(){
+		Destroy (gameObject);
+	}
+
 }
