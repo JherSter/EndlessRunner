dd428f404ddb87895d979709dfa1ebaa5a8342a3
JherSter <jeroenavis123@gmail.com>
2016-10-14 15:10:17 +0200

    Timepickup added

--- Assets/scripts/SpeedPickup.cs
+++ Assets/scripts/SpeedPickup.cs
@@ -3,23 +3,28 @@ using System.Collections;
 
 public class SpeedPickup : MonoBehaviour {
 	
-	private PlayerMovement playerMovement;
+
+	private distance_counter TimeLeft;
+
+
 
 
 	void Start(){
 		
-		//Player = GameObject.FindGameObjectWithTag ("Player");
-		playerMovement = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerMovement> ();
-
+		//Player = GObject.FindGameObjectWithTag ("Player");
+		//TimeLeft = GameObject.FindGameObjectWithTag ("Canvas").GetComponent<Text> ();
+		TimeLeft = GameObject.Find("Canvas").GetComponentInChildren<distance_counter>();
 
 	}
 
 		void OnCollisionEnter(Collision coll) {
 			Debug.Log(coll.gameObject.tag);
+		Debug.Log("NIELS IS LElIJK");
 
 
 		if (coll.gameObject.tag == "Player") {
 			Debug.Log("ello");
+			TimeLeft.targetTime += 10f;
 			
 
 			Destroy (gameObject);
